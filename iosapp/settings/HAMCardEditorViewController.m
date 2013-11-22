9664970139795bcf1e824777d63901606713c04e
Zhang Lei <zhanglei.april@gmail.com>
2013-11-22 19:12:09 +0800

    some bugs fixed, but not all of them

--- iosapp/settings/HAMCardEditorViewController.m
+++ iosapp/settings/HAMCardEditorViewController.m
@@ -10,6 +10,9 @@
 
 @interface HAMCardEditorViewController ()
 
+@property NSString *imagePath;
+@property NSString *tempImagePath;
+
 @end
 
 @implementation HAMCardEditorViewController
@@ -32,14 +35,30 @@
 	// fit into the popover
 	self.preferredContentSize = self.view.frame.size;
 	
+	// initialize the temporary card
 	if (self.cardID) { // editing card
 		self.tempCard = [self.config card:self.cardID];
-		self.cardNameField.text = self.tempCard.name;
-		self.imageView.image = [UIImage imageWithContentsOfFile:[HAMFileTools filePath:self.tempCard.image.localPath]];
 	}
 	else { // creating card
 		self.tempCard = [[HAMCard alloc] initNewCard]; // get a UUID
-
+		[self.config newCardWithID:self.tempCard.UUID name:nil type:1 audio:nil image:nil]; // type 1 indicates a card
+		self.tempCard.type = 1; // FIXME: why should I set this attribute MANUALLY?
+	}
+	
+	self.imagePath = [NSString stringWithFormat:@"%@.jpg", self.tempCard.UUID];
+	self.tempImagePath = [NSString stringWithFormat:@"%@-temp.jpg", self.tempCard.UUID];
+	// update the view accordingly
+	if (self.cardID) { // editing card
+		// copy the existing image file to the temporary
+		// FIXME: error handling
+		NSFileManager *manager = [NSFileManager defaultManager];
+		[manager copyItemAtPath:[HAMFileTools filePath:self.imagePath] toPath:[HAMFileTools filePath:self.tempImagePath] error:nil];
+		
+		self.cardNameField.text = self.tempCard.name;
+		self.tempCard.image.localPath = self.tempImagePath; // point to the temporary file
+		self.imageView.image = [UIImage imageWithContentsOfFile:[HAMFileTools filePath:self.tempCard.image.localPath]];
+	}
+	else { // new card
 		// don't allow deletion when creating a card
 		self.deleteCardButton.hidden = YES;
 		// must specify card name and image before saving
@@ -48,14 +67,10 @@
 		self.recordButton.enabled = NO;
 	}
 	
-	self.cardNameChanged = NO;
-	self.cardImageChanged = NO;
-	
 	// initialize the recorder
 	self.recorder = [[HAMRecorderViewController alloc] initWithNibName:@"HAMRecorderViewController" bundle:nil];
 	self.recorder.config = self.config;
 	self.recorder.tempCard = self.tempCard;
-	self.recorder.cardID = self.cardID;
 	self.recorder.categoryID = self.categoryID;
 }
 
@@ -71,7 +86,6 @@
 	if (! self.cardID) { // creating card
 		if (! [textField.text isEqualToString:@""]) {
 			self.tempCard.name = textField.text;
-			self.cardNameChanged = YES;
 		}
 	}
 	else { // editing card
@@ -80,18 +94,22 @@
 			textField.text = oldCardName;
 		else if (! [textField.text isEqualToString:oldCardName]) {
 			self.tempCard.name = textField.text;
-			self.cardNameChanged = YES;
 		}
 	}
 	
 	// can save new card now
-	self.finishButton.enabled = (self.cardNameChanged && self.cardImageChanged) ? YES : NO;
+	self.finishButton.enabled = (self.tempCard.name && self.tempCard.image) ? YES : NO;
 	self.recordButton.enabled = self.finishButton.enabled;
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
 	[textField resignFirstResponder];
 	return  NO;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+	// disable finish button while editing
+	self.finishButton.enabled = NO;
 }
 
 - (IBAction)recordButtonTapped:(id)sender {
@@ -138,20 +156,20 @@
 	UIImage *tempImage = [info objectForKey:UIImagePickerControllerOriginalImage];
 	self.imageView.image = tempImage; // update the displaying
 	
-	NSString *tempImagePath = [[NSString alloc] initWithFormat:@"%@-temp.jpg", self.tempCard.UUID];
-	BOOL success = [UIImageJPEGRepresentation(tempImage, 1.0) writeToFile:[HAMFileTools filePath:tempImagePath] atomically:YES];
+	// save the image to a temporary file
+	BOOL success = [UIImageJPEGRepresentation(tempImage, 1.0) writeToFile:[HAMFileTools filePath:self.tempImagePath] atomically:YES];
 	if (!success) { // something wrong
 		UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"无法选取图片" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles: nil];
 		[alert show];
 		return; //cannot create new card
 	}
 	else {
-		self.tempCard.image = [[HAMResource alloc] initWithPath:tempImagePath];
-		self.cardImageChanged = YES;
+		if (! self.tempCard.image)
+			self.tempCard.image = [[HAMResource alloc] initWithPath:self.tempImagePath];
 	}
 	
 	// can save new card now
-	if (self.cardNameChanged) {
+	if (self.tempCard.name) {
 		self.finishButton.enabled = YES;
 		self.recordButton.enabled = YES;
 	}
@@ -160,34 +178,24 @@
 }
 
 - (IBAction)cancelButtonTapped:(id)sender {
+	if (! self.cardID) // cancel card creation
+		[self.config deleteCard:self.tempCard.UUID];
+	
 	[self.popover dismissPopoverAnimated:YES];
 }
 
 - (IBAction)finishButtonTapped:(id)sender {
-	if (self.cardID) { // editing card
 		
-		if (self.cardImageChanged) { // image was modified
-			NSFileManager *manager = [NSFileManager defaultManager];
-			NSString *imagePath = [NSString stringWithFormat:@"%@.jpg", self.tempCard.name];
-			// copy and then delete the temporary image file
-			[manager copyItemAtPath:[HAMFileTools filePath:self.tempCard.image.localPath] toPath:[HAMFileTools filePath:imagePath] error:nil];
-			[manager removeItemAtPath:[HAMFileTools filePath:self.tempCard.image.localPath] error:nil];
-			
-			self.tempCard.image.localPath = imagePath; // point to the new image path
-			
-			[self.config updateCard:self.tempCard name:self.tempCard.name audio:self.tempCard.audio.localPath image:self.tempCard.image.localPath];
-		}
-		
-		if (self.cardNameChanged) { // card name was modified
-			[self.config updateCard:self.tempCard name:self.tempCard.name audio:self.tempCard.audio.localPath image:self.tempCard.image.localPath];
-		}
-	}
-	else { // creating card
-		// create the card *into the database*
-		// FIXME: why can't I just use the update card method
-		[self.config newCardWithID:self.tempCard.UUID name:self.tempCard.name type:1 audio:self.tempCard.audio.localPath image:self.tempCard.image.localPath]; // type 1 indicates a card
-		
-		// insert this card to a category
+	// FIXME: error handling
+	NSFileManager *manager = [NSFileManager defaultManager];
+	// copy and then delete the temporary image file
+	[manager copyItemAtPath:[HAMFileTools filePath:self.tempImagePath] toPath:[HAMFileTools filePath:self.imagePath] error:nil];
+	[manager removeItemAtPath:[HAMFileTools filePath:self.tempImagePath] error:nil];
+	
+	self.tempCard.image.localPath = self.imagePath;
+	[self.config updateCard:self.tempCard name:self.tempCard.name audio:self.tempCard.audio.localPath image:self.tempCard.image.localPath];
+	
+	if (! self.cardID) { // if this is a new card, then insert it into a category
 		NSString *categoryID = self.categoryID ? self.categoryID : UNCATEGORIZED_ID; // default uncategorized
 		NSInteger numChildren = [self.config childrenCardIDOfCat:categoryID].count;
 		HAMRoom *room = [[HAMRoom alloc] initWithCardID:self.tempCard.UUID animation:ROOM_ANIMATION_NONE];
@@ -195,13 +203,13 @@
 	}
 	
 	[self.popover dismissPopoverAnimated:YES];
-	[self.delegate cardEditorDidEndEditing:self];
+	[self.delegate cardEditorDidEndEditing:self]; // inform the grid view to refresh
 }
 
 - (IBAction)deleteCardButtonTapped:(id)sender {
 	[self.config deleteCard:self.cardID];
 	[self.popover dismissPopoverAnimated:YES];
-	[self.delegate cardEditorDidEndEditing:self];
+	[self.delegate cardEditorDidEndEditing:self]; // inform the grid view to refresh
 }
 
 @end
