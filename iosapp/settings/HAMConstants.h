3870dd895e5583c9c208e069574da0e8b3c4a212
Zhang Lei <zhanglei.april@gmail.com>
2013-12-07 09:56:40 +0800

    remove some unused constants from HAMConstants.h

--- iosapp/settings/HAMConstants.h
+++ iosapp/settings/HAMConstants.h
@@ -9,15 +9,6 @@
 #ifndef iosapp_Constants_h
 #define iosapp_Constants_h
 
-extern const CGFloat GRID_VIEW_WIDTH;
-extern const CGFloat GRID_VIEW_HEIGHT;
-extern const CGFloat CELL_WIDTH;
-extern const CGFloat CELL_HEIGHT;
-extern const CGFloat INTER_ITEM_SPACING;
-extern const CGFloat LINE_SPACE;
-
-extern const NSInteger COLLECTION_VIEW_TAG;
-
 extern const CGFloat SCREEN_WIDTH;
 extern const CGFloat SCREEN_HEIGHT;
 
